d45ef75f1aa799f82de3642d021613eddf850ed8
Tóth Péter Attila <h378716@stud.u-szeged.hu>
2025-04-14 14:07:02 +0200

    létrehozva

---
+++ src/main/java/com/adatb/repjegy_fogalas/RepjegyFogalasApplication.java
@@ -0,0 +1,13 @@
+package com.adatb.repjegy_fogalas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepjegyFogalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepjegyFogalasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adatb/repjegy_fogalas/RepjegyFogalasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adatb.repjegy_fogalas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepjegyFogalasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
